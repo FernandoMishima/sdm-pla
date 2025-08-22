1df9cc2eb49d606f5d0192c84cc8b1de82f7bb8b
Fernando Toshio Oliveira Mishima <822124385@ulife.com.br>
2025-08-22 11:13:25 -0300

    Conhecendo variáveis em Java Script

---
+++ variaveis.js
@@ -0,0 +1,55 @@
+var nome = "Fernando"
+const altura = 1.75
+let idade = 21
+//altura = 1.80
+idade = 25
+console.log("Meu nome é " + nome + ", minha altura é " + altura +" e tenho "+ idade + " anos.")
+
+// String
+
+let texto = "String"
+
+// Number
+
+let numero = 10
+let numero = 10.5
+
+// Boolean
+
+let booleano = True
+let booleano2 = False
+
+// null
+
+let nulo = null
+
+// undefined
+
+let indefinido = undefined
+
+// object
+
+let objeto = {nome: "Fernando", idade: 21, altura: 1.75}
+
+// array
+
+lista = ["uva", "maçã", "banana "]
+
+// Conversão de tipos de dados
+
+// String -> Número
+
+let stringParaNumero = Number("10")
+let stringParaNumero2 = parseInt("10.5") // converte pra inteiro
+let stringParaNumero3 = parseFloat("10.5") // converte pra float
+
+// Número -> String
+
+let str = string(100)
+let str2 = (100).toString()
+
+// Boolean -> Número
+
+let booleanParaNumero = Number(true) // 1
+let booleanParaNumero2 = Number(false) // 0
+
